4d56bc6a24f4baa0e3ac24619dcf75ec115330b9
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-03-05 18:24:31 -0500

    Add struct + impl for reading from serial port

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,55 @@
-use serial::{self, PortSettings, SerialPort};
+use serial::{self, unix::TTYPort, PortSettings, SerialPort};
 use std::io::Read;
 
 const FLAT: u8 = 48;
 const VERTICAL: u8 = 49;
 
-fn main() -> Result<(), std::io::Error> {
-    let mut port = serial::open("/dev/ttyACM0")?;
-    port.configure(&PortSettings {
-        baud_rate: serial::Baud9600,
-        char_size: serial::Bits8,
-        parity: serial::ParityNone,
-        stop_bits: serial::Stop1,
-        flow_control: serial::FlowNone,
-    })?;
+// How many times in a row the arduino has to register
+// a value before considering it consistent
+const TILT_THRESHOLD: i32 = 500;
 
-    let mut buf = [0; 1];
-    loop {
-        if let Ok(_) = port.read_exact(&mut buf) {
-            println!(
-                "{}",
-                match buf[0] {
-                    FLAT => "0",
-                    VERTICAL => "1",
-                    _ => "?",
-                }
-            );
-        }
+struct TiltReader {
+    buf: [u8; 1],
+    port: TTYPort,
+}
+impl TiltReader {
+    fn new() -> Result<TiltReader, std::io::Error> {
+        let mut port = serial::open("/dev/ttyACM0")?;
+        port.configure(&PortSettings {
+            baud_rate: serial::Baud9600,
+            char_size: serial::Bits8,
+            parity: serial::ParityNone,
+            stop_bits: serial::Stop1,
+            flow_control: serial::FlowNone,
+        })?;
+
+        Ok(TiltReader { buf: [0; 1], port })
     }
 
-    // Ok(())
+    fn determine_position(&mut self) -> u8 {
+        let mut tilt_length = 0;
+        let mut current_orientation = FLAT;
+
+        while tilt_length < TILT_THRESHOLD {
+            if let Ok(_) = self.port.read_exact(&mut self.buf) {
+                if self.buf[0] == current_orientation {
+                    tilt_length += 1;
+                } else {
+                    current_orientation = self.buf[0];
+                    tilt_length = 0;
+                }
+            }
+        }
+
+        return current_orientation;
+    }
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let mut reader = TiltReader::new()?;
+
+    loop {
+        let pos = reader.determine_position();
+        println!("{}", pos);
+    }
 }
